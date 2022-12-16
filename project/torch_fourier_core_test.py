a11075b826f46f3f54382902a70edae1d02d712c
trevormccrt <trevormccrt@gmail.com>
2022-12-16 16:27:18 -0500

    transforms

---
+++ project/torch_fourier_core_test.py
@@ -0,0 +1,18 @@
+import numpy as np
+import torch
+
+from project import fourier_core, torch_fourier_core
+
+
+def test_transform_equivalence():
+    x_dims = (2**6, 2**7, 2**5, 2**4)
+    axis_from = np.random.randint(0, len(x_dims))
+    x = np.random.uniform(-1, 1, x_dims)
+    scipy_coeffs = fourier_core.fourier_series_coeffs(x, axis_from)
+    scipy_ifft = fourier_core.nifft(x, axis_from)
+    with torch.no_grad():
+        torch_coeffs = torch_fourier_core.fourier_series_coeffs(torch.from_numpy(x), axis_from)
+        torch_ifft = torch_fourier_core.nifft(torch.from_numpy(x), axis_from)
+    np.testing.assert_allclose(scipy_coeffs, torch_coeffs)
+    np.testing.assert_allclose(scipy_ifft, torch_ifft)
+
